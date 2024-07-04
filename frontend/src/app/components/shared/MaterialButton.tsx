788064b0812c7096658e09b57240e7b68fa4090a
Kateryna Rodina <katerynarodina@katerynas-mbp.mynetworksettings.com>
2024-07-04 19:58:26 -0400

    add components lib

---
+++ frontend/src/app/components/shared/MaterialButton.tsx
@@ -0,0 +1,9 @@
+// src/app/MaterialButton.tsx
+"use client"; // This directive makes this file a client component
+
+import { Button } from "@material-tailwind/react";
+
+export function ButtonDefault() {
+    // @ts-ignore
+    return <Button size="sm">Test button</Button>;
+  }
